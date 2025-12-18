f20a7f751dd6ad1e0c80e1deb8d64b0d8c16dad0
Tech4Gofficial <tech.4g.official00@gmail.com>
2025-12-18 11:46:06 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDvtXvL5Zp3WCz8udqxe2E_kNP7D1oyryE",
+  authDomain: "game-3515c.firebaseapp.com",
+  databaseURL: "https://game-3515c-default-rtdb.firebaseio.com",
+  projectId: "game-3515c",
+  storageBucket: "game-3515c.firebasestorage.app",
+  messagingSenderId: "904642888900",
+  appId: "1:904642888900:web:fbcd99b0dec3065c78b591"
 };
 
 firebase.initializeApp(firebaseConfig);
